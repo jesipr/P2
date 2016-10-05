d53b9c35c5f8545d690a9123f1e83a90ebdc187f
Jesi <jesi.nieves@gmail.com>
2016-10-05 19:38:32 -0400

    BrainStorming
    Just brain stroming ideas.

--- src/p2MainClasses/DataFilePopulator.java
+++ src/p2MainClasses/DataFilePopulator.java
@@ -1,10 +1,46 @@
 package p2MainClasses;
 
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
+import java.util.Scanner;
+
 public class DataFilePopulator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		DataFilePopulator d = new DataFilePopulator();
+System.out.println("Finnished");
+	}
 
+	RandomAccessFile raf;
+
+	public DataFilePopulator() {
+
+		try {
+			raf = new RandomAccessFile(System.getProperty("user.dir") + "/inputData/rand.txt", "rw");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		System.out.println("File created");
+		System.out.println("Enter Name for column, -1 to finish");
+		Scanner s = new Scanner(System.in);
+		String st = s.next();
+
+		int m = 0;
+		while (m > -1) {
+			try {
+				if (Integer.parseInt(st) < 0) {
+					m = Integer.parseInt(st);
+				}
+			} catch (NumberFormatException e) {
+				e.printStackTrace();
+			}
+			
+			
+		}
+
+		s.close();
 	}
 
 }
