6eb9efaa7c0e26068ae1d2ac72c125fd4f891918
Jesi <jesi@Jesiniels-MacBook-Pro.local>
2016-10-05 12:46:05 -0400

    Initial Commit
    Added .gitignore and the “skeleton” of the Project.

---
+++ src/p2MainClasses/DataFilePopulator.java
@@ -0,0 +1,10 @@
+package p2MainClasses;
+
+public class DataFilePopulator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/p2MainClasses/TableAnalyzer.java
@@ -0,0 +1,10 @@
+package p2MainClasses;
+
+public class TableAnalyzer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
