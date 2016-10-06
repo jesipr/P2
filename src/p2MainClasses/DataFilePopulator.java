17e29c02c73c389f6964e8b5f78d6703909ac665
Jesi <jesi.nieves@gmail.com>
2016-10-05 22:00:41 -0400

    Testing Arguments
    Arguments testing

--- src/p2MainClasses/DataFilePopulator.java
+++ src/p2MainClasses/DataFilePopulator.java
@@ -1,46 +1,18 @@
 package p2MainClasses;
 
-import java.io.FileNotFoundException;
-import java.io.RandomAccessFile;
-import java.util.Scanner;
-
 public class DataFilePopulator {
 
 	public static void main(String[] args) {
-		DataFilePopulator d = new DataFilePopulator();
-System.out.println("Finnished");
-	}
-
-	RandomAccessFile raf;
-
-	public DataFilePopulator() {
-
-		try {
-			raf = new RandomAccessFile(System.getProperty("user.dir") + "/inputData/rand.txt", "rw");
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		if (args.length!=1){ 
+			System.out.println("Illegal Argument. Try again");
+			System.exit(0);
+		}else{
+			System.out.println(args[0]);
 		}
+		
+		
+		
 
-		System.out.println("File created");
-		System.out.println("Enter Name for column, -1 to finish");
-		Scanner s = new Scanner(System.in);
-		String st = s.next();
-
-		int m = 0;
-		while (m > -1) {
-			try {
-				if (Integer.parseInt(st) < 0) {
-					m = Integer.parseInt(st);
-				}
-			} catch (NumberFormatException e) {
-				e.printStackTrace();
-			}
-			
-			
-		}
-
-		s.close();
 	}
 
 }
